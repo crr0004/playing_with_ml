e3c269b5e43b3425c024f77e9fa78a3d7db5f4ee
crr0004 <crr0004@gmail.com>
2020-01-25 16:46:48 +1100

    Trying to figure out gradient descent; EOD push

--- Tensorflow 2.0 Backpropagation.py
+++ Tensorflow 2.0 Backpropagation.py
@@ -29,22 +29,42 @@ def run(input, exceptedOutput, layerDefinitions):
                 (layerDefinitions[2], layerDefinitions[1]), 
                 dtype=dtype
             )
-        layer2Bias = tensorInitializer(
+        layer1Bias = tensorInitializer(
                 (layerDefinitions[1], 1), 
                 dtype=dtype
             )
-        layer3Bias = tensorInitializer(
+        layer2Bias = tensorInitializer(
                 (layerDefinitions[2], 1), 
                 dtype=dtype
             )
 
-        layer1Z = tf.matmul(layer1ToLayer2Weights, inputTensor)+layer2Bias
+        layer1Z = tf.matmul(layer1ToLayer2Weights, inputTensor)+layer1Bias
         # Reluu activation
         layer1Activation = tf.math.maximum(layer1Z, 0)
 
-        layer2Z = tf.matmul(layer2ToLayer3Weights, layer1Activation)+layer3Bias
+        layer2Z = tf.matmul(layer2ToLayer3Weights, layer1Activation)+layer2Bias
         layer2Activation = tf.math.maximum(layer2Z, 0)
         outputDifference = tf.math.squared_difference(layer2Activation, outputTensor)
+
+        # Could be called output cost as well. Layer2 is just the final layer here
+        # do an element wise multiplication
+        # might be able to do a booleanmask here ¯\_(ツ)_/¯
+        outputCost = tf.math.multiply(outputDifference, 
+            # This is a lazy way of implementing Reluu differentiation
+            # Basically turn everything to a True or false, with True being > 0,
+            # then turn it all back into numbers.
+            tf.cast(layer2Activation > 0, dtype)
+        )
+        layer2BiasDelta = outputCost
+        layer2WeightDelta = tf.matmul(outputCost, layer2Activation, transpose_b=True)
+
+        layer1Cost = tf.math.multiply(
+            tf.matmul(layer2ToLayer3Weights, outputCost, transpose_a=True),
+            tf.cast(layer1Activation > 0, dtype)
+            )
+        
+        layer1BiasDelta = layer1Cost
+        layer1WeightDelta = tf.matmul(layer1Cost, layer1Activation, transpose_b=True)
 
 
 
